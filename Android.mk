2e073fe7cb40515b3c39509dea8d81ffe29d3b03
Bernhard Rosenkränzer <Bernhard.Rosenkranzer@linaro.org>
2013-02-14 00:13:48 +0100

    native-toolchain: Try harder to make system.img generation depend on us
    Signed-off-by: Bernhard Rosenkränzer <Bernhard.Rosenkranzer@linaro.org>

--- Android.mk
+++ Android.mk
@@ -2,13 +2,21 @@
 # This (obviously) lacks proper dependency tracking. If you need to rebuild,
 # rm -f $(PRODUCT_OUT)/system/bin/gcc.
 
-native-toolchain: $(PRODUCT_OUT)/system/bin/gcc
+GCC_FILE_NAME = $(PRODUCT_OUT)/system/bin/gcc
 
-$(PRODUCT_OUT)/system/bin/gcc: $(TARGET_CRTBEGIN_DYNAMIC_O) $(TARGET_CRTEND_O) $(TARGET_OUT_SHARED_LIBRARIES)/libm.so $(TARGET_OUT_SHARED_LIBRARIES)/libc.so $(TARGET_OUT_SHARED_LIBRARIES)/libdl.so $(TARGET_OUT_SHARED_LIBRARIES)/libstlport.so
+native-toolchain: $(GCC_FILE_NAME)
+
+$(GCC_FILE_NAME): $(TARGET_CRTBEGIN_DYNAMIC_O) $(TARGET_CRTEND_O) $(TARGET_OUT_SHARED_LIBRARIES)/libm.so $(TARGET_OUT_SHARED_LIBRARIES)/libc.so $(TARGET_OUT_SHARED_LIBRARIES)/libdl.so $(TARGET_OUT_SHARED_LIBRARIES)/libstlport.so
 	DEST=$(realpath $(TOP))/$(PRODUCT_OUT) CRT=$(realpath $(shell dirname $(TOP)/$(TARGET_CRTBEGIN_DYNAMIC_O))) HOST_OUT=$(HOST_OUT) INTREE=true $(CURDIR)/native-toolchain/build.sh
 
-systemimage: native-toolchain
+systemimage: $(GCC_FILE_NAME)
 
-systemtarball: native-toolchain
+systemtarball: $(GCC_FILE_NAME)
+
+$(BUILT_SYSTEMIMAGE): $(GCC_FILE_NAME)
+
+$(INSTALLED_SYSTEMIMAGE): $(GCC_FILE_NAME)
+
+FULL_SYSTEMIMAGE_DEPS += $(GCC_FILE_NAME)
 
 .PHONY: native-toolchain
